97037c4bcbaaf9f9fb4272cb995d0e560cc3d1c6
Jeovani Bianchini <bianchini.jeovani@gmail.com>
2022-11-28 13:59:33 -0400

    first commit

---
+++ backend/jbcatalog/src/test/java/com/bianchini/jbcatalog/JbcatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bianchini.jbcatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JbcatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
